526a1f466e20677309a70ac2f21371d310dca060
mulshine <mulshine@princeton.edu>
2017-12-05 16:32:43 -0500

    Getting rid of debug folder.

--- genera_test/Debug/Src/OOPSMath.d
+++
@@ -1,8 +0,0 @@
-Src/OOPSMath.o: ../Src/OOPSMath.c ../Src/../Inc/OOPSMath.h \
- ../Src/../Inc/OOPSWavetables.h ../Src/../Inc/OOPSMemConfig.h
-
-../Src/../Inc/OOPSMath.h:
-
-../Src/../Inc/OOPSWavetables.h:
-
-../Src/../Inc/OOPSMemConfig.h:
